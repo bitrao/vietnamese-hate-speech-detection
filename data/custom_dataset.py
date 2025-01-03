6a2c5cfd10f5e83b5b4ac622fec8dbad87830cb1
Nguyen Bao Tri <trinb@Mac-mini-cua-Nguyen.local>
2025-01-03 17:57:10 +0700

    add data

---
+++ data/custom_dataset.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length=128):
+        """
+        Dataset for text classification.
+        
+        Args:
+            texts: List of input texts
+            labels: List of labels (0: normal, 1: hate speech)
+            tokenizer: HuggingFace tokenizer
+            max_length: Maximum sequence length
+        """
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, idx):
+        text = str(self.texts[idx])
+        
+        encoding = self.tokenizer(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+        
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'label': torch.tensor(self.labels[idx], dtype=torch.long)
+        }
